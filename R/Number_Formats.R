3aecaa677cf26f10a1bec1b68d9038b2e0ecde90
Aaron Smith <aqsmith08@gmail.com>
2016-01-28 21:07:08 +0100

    Create FormatBigNumbers function
    Follows Google's R Style Guide -- https://google.github.io/styleguide/Rguide.xml#functiondefinition.

--- R/Number_Formats.R
+++ R/Number_Formats.R
@@ -20,3 +20,21 @@ nettle_bignumbers <- function(x) {
   return(output)
 }
 
+FormatBigNumbers <- function(num) {
+	# Formats a number with more than 6 digits into an abbreviation string
+	#
+	# Args:
+	#   num: A number of at least 6 digits
+	#
+	# Returns:
+	#   A string of the number abbreviated to "M", "B", or "T"
+	t.index = num >= 1e12
+	b.index = num >= 1e9 & num < 1e12
+	m.index = num >= 1e5 & num < 1e9
+	output = formatC(num, format = "d", big.mark = ",")
+	
+	output[t.index] = paste(formatC(num[t.index] / 1e12, digits = 1,format = "f"), "T")
+	output[b.index] = paste(formatC(num[b.index] / 1e9, digits = 1, format = "f"), "B")
+	output[m.index] = paste(formatC(num[m.index] / 1e6, digits = 1, format = "f"), "M")
+	return(output)
+}
